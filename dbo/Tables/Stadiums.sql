7f586203c19a266c2f77c86c1a223fe72fd27703
northernman86 <aliaksandrprystauka@gmail.com>
2022-05-21 22:35:25 +0300

    Add table.

--- dbo/Tables/Stadiums.sql
+++ dbo/Tables/Stadiums.sql
@@ -3,7 +3,7 @@
 	[StadiumId] INT PRIMARY KEY IDENTITY (1, 1) NOT NULL, 
     [CityId] INT NOT NULL, 
     [NameStadium] NVARCHAR(50) NOT NULL, 
-    [Capacity] NVARCHAR(50) NOT NULL, 
+    [Capacity] INT NOT NULL, 
     [YearConstruction] DATE NOT NULL, 
     CONSTRAINT [FK_Stadiums_Cities] FOREIGN KEY ([CityId]) REFERENCES [Cities]([CityId]) ON UPDATE CASCADE
 )
