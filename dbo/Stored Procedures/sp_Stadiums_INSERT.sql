cc05a800417e32f7eb37295e5410a624caef093f
northernman86 <aliaksandrprystauka@gmail.com>
2022-05-21 22:40:51 +0300

    Add stored procedure.

---
+++ dbo/Stored Procedures/sp_Stadiums_INSERT.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [dbo].[sp_Stadiums_INSERT]
+	@cityId INT,
+	@name NVARCHAR(50),
+	@capacity INT,
+	@year DATE
+AS
+BEGIN
+	INSERT INTO Stadiums(CityId, NameStadium, Capacity, YearConstruction)
+	VALUES(@cityId, @name, @capacity, @year)
+END;
