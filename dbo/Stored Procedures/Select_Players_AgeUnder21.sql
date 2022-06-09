b5c88ea1332dc0c6954ba663c3e23cd867759cb0
northernman86 <aliaksandrprystauka@gmail.com>
2022-06-09 22:24:23 +0300

    Add stored procedures.

---
+++ dbo/Stored Procedures/Select_Players_AgeUnder21.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [dbo].[Select_Players_AgeUnder21]
+AS
+BEGIN
+	SELECT Players.NamePlayer, Players.SurnamePlayer, Players.AgePlayer, Nationalities.Nationality, FootballClubs.NameFC, RolePlayers.RolePlayer
+	FROM Players
+	INNER JOIN Nationalities ON Players.NationId = Nationalities.NationId
+	INNER JOIN FootballClubs ON Players.FCId = FootballClubs.FCId
+	INNER JOIN RolePlayers ON Players.RolePlayerId = RolePlayers.RolePlayerId
+	WHERE AgePlayer < 21
+	ORDER BY AgePlayer
+END;
---
+++ dbo/Stored Procedures/Select_Referees_AgeOver45.sql
@@ -0,0 +1,9 @@
+﻿CREATE PROCEDURE [dbo].[Select_Referees_AgeOver45]
+AS
+BEGIN
+	SELECT ClassificationReferees.NameClassReferee, Referees.NameReferee, Referees.SurnameReferee, Referees.AgeReferee
+	FROM Referees
+	INNER JOIN ClassificationReferees ON Referees.ClassRefereeId = ClassificationReferees.ClassRefereeId
+	WHERE AgeReferee > 45
+	ORDER BY AgeReferee
+END;
