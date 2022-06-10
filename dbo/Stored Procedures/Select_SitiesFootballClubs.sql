dec3ba1227b9a4ab1eb4f9f6179c9bdf999441c0
northernman86 <aliaksandrprystauka@gmail.com>
2022-06-10 15:15:30 +0300

    Add sored procedure.

---
+++ dbo/Stored Procedures/Select_SitiesFootballClubs.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROCEDURE [dbo].[Select_SitiesFootballClubs]
+AS
+BEGIN
+	SELECT Cities.NameCity, FootballClubs.NameFC
+	FROM Cities
+	INNER JOIN FootballClubs ON Cities.CityId = FootballClubs.CityId
+	ORDER BY NameCity
+END;
