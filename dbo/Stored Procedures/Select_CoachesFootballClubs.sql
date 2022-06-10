e111c4b4b4de5bba37eaa98f325f0ec588b992f2
northernman86 <aliaksandrprystauka@gmail.com>
2022-06-11 01:34:44 +0300

    Add stored procedures.

---
+++ dbo/Stored Procedures/Select_CoachesFootballClubs.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [dbo].[Select_CoachesFootballClubs]
+AS
+BEGIN
+	SELECT FootballClubs.NameFC, ClassificationCoaches.NameClassCoach, Coaches.SurnameCoach, Coaches.NameCoach, Nationalities.Nationality
+	FROM Coaches
+	INNER JOIN FootballClubs ON Coaches.FCId = FootballClubs.FCId
+	INNER JOIN ClassificationCoaches ON Coaches.ClassCoachId = ClassificationCoaches.ClassCoachId
+	INNER JOIN Nationalities ON Coaches.NationId = Nationalities.NationId
+	ORDER BY NameFC, NameClassCoach
+END;
---
+++ dbo/Stored Procedures/Select_PlayersFootballClubs.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [dbo].[Select_PlayersFootballClubs]
+AS
+BEGIN 
+	SELECT FootballClubs.NameFC, RolePlayers.RolePlayer, Players.SurnamePlayer, Players.NamePlayer, Nationalities.Nationality
+	FROM Players
+	INNER JOIN FootballClubs ON Players.FCId = FootballClubs.FCId
+	INNER JOIN RolePlayers ON Players.RolePlayerId = RolePlayers.RolePlayerId
+	INNER JOIN Nationalities ON Players.NationId = Nationalities.NationId
+	ORDER BY NameFC, RolePlayer
+END;
