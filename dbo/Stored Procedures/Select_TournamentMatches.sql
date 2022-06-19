59a258ce458001e841fa6ee4665b0d98c331e1ec
northernman86 <aliaksandrprystauka@gmail.com>
2022-06-19 21:39:24 +0300

    Add stored procedure.

---
+++ dbo/Stored Procedures/Select_TournamentMatches.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [dbo].[Select_TournamentMatches]
+AS
+BEGIN 
+	SELECT Tournaments.NameTournament, Matches.FC1Id, Matches.FC2Id
+	FROM Matches
+	INNER JOIN Tournaments ON Matches.TournamentId = Matches.TournamentId
+	INNER JOIN FootballClubs ON Matches.FC1Id = FootballClubs.FCId
+							AND Matches.FC2Id = FootballClubs.FCId
+	ORDER BY NameTournament
+END;
