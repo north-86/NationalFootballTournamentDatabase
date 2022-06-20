185f2089419f0c6cda21d27cc515671fa3f5bb15
northernman86 <aliaksandrprystauka@gmail.com>
2022-06-20 22:48:58 +0300

    Add stored procedure.

---
+++ dbo/Stored Procedures/Select_NewestFootballClub.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROCEDURE [dbo].[Select_NewestFootballClub]
+AS
+BEGIN
+	SELECT FootballClubs.NameFC, FootballClubs.YearFoundation
+	FROM FootballClubs
+	WHERE YearFoundation IN (SELECT MAX(YearFoundation)
+							FROM FootballClubs)
+END;
