1f2b3466279687c4a09b8964bea6434b888b2faf
northernman86 <aliaksandrprystauka@gmail.com>
2022-06-21 20:38:36 +0300

    Add stored procedure.

---
+++ dbo/Stored Procedures/Select_RefereeAllowedAppointedMatchByLocation.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [dbo].[Select_RefereeAllowedAppointedMatchByLocation]
+	@fcId INT
+AS
+BEGIN
+	SELECT Referees.SurnameReferee, Referees.NameReferee, Cities.NameCity
+	FROM Referees
+	INNER JOIN Cities ON Referees.CityId = Cities.CityId
+	INNER JOIN FootballClubs ON Referees.CityId != FootballClubs.CityId
+	WHERE FootballClubs.FCId = @fcId
+	ORDER BY NameCity
+END;
